da1d8acb7ec970af1f8df060f064f9581e555d17
william gomes de souza <william.xbox@live.com>
2024-04-25 15:53:25 -0300

    terminei aula 66

--- Aula66_FilterArray/index.js
+++ Aula66_FilterArray/index.js
@@ -1,4 +1,4 @@
-// Filter, map 'modificar', reduce 'reduzir em um unico valor'
+// Filter, map 'mapeando', reduce 'reduzir em um unico valor'
 
 //retorne os numeros maiores que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
@@ -7,24 +7,25 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
     else{return false;}
     return valor > 10;
 }
+
 //const filtrados = numeros.filter(callbackFilter);*/
+const pessoas = [
+    {nome: 'William', idade:21},
+    {nome: 'Wilson', idade:51},
+    {nome: 'Wilder', idade:17},
+    {nome: 'Ana', idade:33},
+    {nome: 'Winderson', idade:31},
+    {nome: 'Maria', idade:80},
+];
 const filtrados = numeros.filter(valor => valor > 10);//uma linha ou retorn
 console.log(filtrados);
 
 //retorne as pessoas que tem o nome com 5 letras ou mais
-const pessoaComNomeGrand = pessoas.filter(function(valor) {
-    
-})
+const pessoaComNomeGrand = pessoas.filter(obj => obj.nome.length >= 7);
 
 //retorne as pessoa com mais de 50 anos
+const pessoaMais50Anos = pessoas.filter(obj => obj.idade > 50);
 
 //retorne as pessoas cujo nome termina com a
-
-const pessoas = [
-    {nome: 'William', idade:21},
-    {nome: 'Wilson', idade:29},
-    {nome: 'Wilder', idade:17},
-    {nome: 'Woshotom', idade:33},
-    {nome: 'Winderson', idade:31},
-    {nome: 'Weder', idade:40},
-];
+const pessoaNomeTerminaComA = pessoas.filter(obj => obj.nome.endsWith('a'))
+console.log(pessoaComNomeGrand,pessoaMais50Anos,pessoaNomeTerminaComA);
